d567c0e5999bd72a4042c5cbcf25300e449feed1
lazorfuzz <leontosy@gmail.com>
2019-07-16 06:54:37 -0700

    Filter out non images in memefinder

--- memefinder.py
+++ memefinder.py
@@ -14,12 +14,13 @@ def find():
   for meme in memes_list:
     try:
       url = meme['data']['url']
-      api_endpoint = os.environ['API_ENDPOINT']
-      post_data = urlencode({'url': url, 'meme_auth': os.environ['AUTH'], 'temp': '1'}).encode('ascii')
-      req = Request(url=api_endpoint, method='POST', data=post_data)
-      with urlopen(req) as client:
-        data = client.read()
-      print(data, url)
+      if '.jpg' in url or '.png' in url or '.jpeg' in url:
+        api_endpoint = os.environ['API_ENDPOINT']
+        post_data = urlencode({'url': url, 'meme_auth': os.environ['AUTH'], 'temp': '1'}).encode('ascii')
+        req = Request(url=api_endpoint, method='POST', data=post_data)
+        with urlopen(req) as client:
+          data = client.read()
+        print('Added', url)
     except: pass
 
 if __name__ == '__main__':
